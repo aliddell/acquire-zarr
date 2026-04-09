cb3f4c12f9fde0ba817c7fc8c1e68787d101b8a9
Alan Liddell <aliddell@chanzuckerberg.com>
2026-04-09 11:22:07 -0400

    Remove some dead code

--- src/streaming/sink.hh
+++ src/streaming/sink.hh
@@ -91,28 +91,6 @@ make_file_sink(std::string_view file_path,
                std::shared_ptr<FileHandlePool> file_handle_pool);
 
 /**
- * @brief Create a collection of file sinks for a Zarr dataset.
- * @param[in] base_path The path to the base directory for the dataset.
- * @param[in] dimensions The dimensions of the dataset.
- * @param[in] parts_along_dimension Function to determine the number of
- * parts (i.e., shards or chunks) along a dimension.
- * @param[in] thread_pool Pointer to a thread pool object. Used to create files
- * in parallel.
- * @param file_handle_pool Pointer to a pool of file handles.
- * @param[out] part_sinks The sinks created.
- * @return True iff all file sinks were created successfully.
- * @throws std::runtime_error if @p base_path is not valid, or if the number
- * of parts along a dimension is zero.
- */
-[[nodiscard]] bool
-make_data_file_sinks(std::string_view base_path,
-                     const ArrayDimensions& dimensions,
-                     const DimensionPartsFun& parts_along_dimension,
-                     std::shared_ptr<ThreadPool> thread_pool,
-                     std::shared_ptr<FileHandlePool> file_handle_pool,
-                     std::vector<std::unique_ptr<Sink>>& part_sinks);
-
-/**
  * @brief Create a sink from an S3 bucket name and object key.
  * @param bucket_name The name of the bucket in which the object is stored.
  * @param object_key The key of the object to write to.
@@ -126,24 +104,4 @@ std::unique_ptr<Sink>
 make_s3_sink(std::string_view bucket_name,
              std::string_view object_key,
              std::shared_ptr<S3ConnectionPool> connection_pool);
-
-/**
- * @brief Create a collection of S3 sinks for a Zarr dataset.
- * @param[in] bucket_name The name of the bucket in which the dataset is
- * stored.
- * @param[in] base_path The path to the base directory for the dataset.
- * @param[in] dimensions The dimensions of the dataset.
- * @param[in] parts_along_dimension Function to determine the number of
- * parts (i.e., shards or chunks) along a dimension.
- * @param[in] connection_pool Pointer to a pool of existing S3 connections.
- * @param[out] part_sinks The sinks created.
- * @return True iff all file sinks were created successfully.
- */
-[[nodiscard]] bool
-make_data_s3_sinks(std::string_view bucket_name,
-                   std::string_view base_path,
-                   const ArrayDimensions& dimensions,
-                   const DimensionPartsFun& parts_along_dimension,
-                   std::shared_ptr<S3ConnectionPool> connection_pool,
-                   std::vector<std::unique_ptr<Sink>>& part_sinks);
 } // namespace zarr
